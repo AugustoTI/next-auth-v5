5fed495406e3a88ebbd3cee5b0362a76283e7a32
AugustoTI <augustocesar4001@gmail.com>
2024-01-13 22:10:23 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+
+import { Button } from '~/components/ui/button'
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-500 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">Auth.JS 🔐</h1>
+        <p className="text-lg text-white [&_b]:underline [&_b]:underline-offset-4">
+          Um <b>Robusto</b>, <b>Flexivel</b> e <b>Open Source</b> serviço de autenticação.
+        </p>
+        <Button size="lg" variant="secondary" asChild>
+          <Link href="/auth/login">Login</Link>
+        </Button>
+      </div>
+    </main>
+  )
 }
